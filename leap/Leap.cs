1dcd8d9dcf72412c7a5c02fe836f9bbef3156ced
jamiefox5 <32071306+jamiefox5@users.noreply.github.com>
2018-02-15 15:13:58 -0600

    Delete Leap.cs

--- leap/Leap.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class Leap
-{
-    public static bool IsLeapYear(int year)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-}
